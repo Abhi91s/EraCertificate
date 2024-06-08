03be662f53d01bf4f4ba641d3b2395b07ee12638
Abhi91s <abhisheklakhore2001@gmail.com>
2024-06-08 13:25:45 +0530

    add structure

---
+++ src/main/java/com/example/eracertificate/EracertificateApplication.java
@@ -0,0 +1,14 @@
+package com.example.eracertificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EracertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EracertificateApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/example/eracertificate/EracertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eracertificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EracertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
