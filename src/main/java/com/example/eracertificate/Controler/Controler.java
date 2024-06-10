32a5a15bed875f2024a0866170ef010474a68490
Sandeep Mahawat <sandeepmahawat09@gmail.com>
2024-06-10 23:46:40 +0530

    form edit karna baaki he

--- src/main/java/com/example/eracertificate/Controler/Controler.java
+++ src/main/java/com/example/eracertificate/Controler/Controler.java
@@ -1,7 +1,12 @@
 package com.example.eracertificate.Controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.eracertificate.BasicCertificates.basic;
+
 
 @Controller
 public class Controler {
@@ -9,5 +14,5 @@ public class Controler {
     public String homepage() {
         return "/Fragments/index";
     }
-
+    
 }
