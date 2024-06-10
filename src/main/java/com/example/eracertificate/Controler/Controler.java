bad296ee95641aef65bbc539e4c6f3d4121a9e73
Sarthak Parulekar <148764141+sarthakdev143@users.noreply.github.com>
2024-06-10 20:14:38 +0530

    "Added Spring Boot Actuator dependency, modified controller classes, updated CSS styles for forms, and added form templates with input fields in HTML file."

--- src/main/java/com/example/eracertificate/Controler/Controler.java
+++ src/main/java/com/example/eracertificate/Controler/Controler.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Controler
-{
-       @GetMapping("/")
-    public String homepage() 
-    { 
+public class Controler {
+    @GetMapping("/")
+    public String homepage() {
         return "/Fragments/index";
     }
-    
+
 }
